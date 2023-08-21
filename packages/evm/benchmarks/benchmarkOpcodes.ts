2517f78b44e7987a52d50e675b85353ce1b5a101
Marcin Stachura <stan7@tlen.pl>
2023-08-21 12:12:41 +0200

    simplified EVM creation

--- packages/evm/benchmarks/benchmarkOpcodes.ts
+++ packages/evm/benchmarks/benchmarkOpcodes.ts
@@ -2,14 +2,8 @@ import { ArgumentParser } from 'argparse'
 import benchmark from 'benchmark'
 import _ from 'lodash'
 import { Dictionary } from 'lodash'
-import { Block } from '@ethereumjs/block'
-import { Blockchain } from '@ethereumjs/blockchain'
-import { Common, Hardfork, ConsensusType, ConsensusAlgorithm } from '@ethereumjs/common'
-import { MemoryLevel } from 'memory-level'
-import { EEI } from '@ethereumjs/vm'
 import { EVM } from '@ethereumjs/evm'
-import { DefaultStateManager } from '@ethereumjs/statemanager'
-import { Address, MAX_INTEGER_BIGINT, KECCAK256_RLP_ARRAY } from '@ethereumjs/util'
+import { Address, hexToBytes } from '@ethereumjs/util'
 
 class Stats {
   runId?: number
@@ -23,48 +17,12 @@ class Stats {
 async function runBenchmark(bytecode: string): Promise<Stats> {
   /**
    * Benchmarking bytecode passed as command line argument.
-   * The goal is to set up EVM to run the code as fast as possible
-   * and without executing unnecessary code.
    *
    * Debug loggers functions are not called if there is no DEBUG env variable set,
    * so we don't have to care about that.
    */
 
-  const common = Common.custom({
-    chainId: 1234,
-    networkId: 1234,
-    defaultHardfork: Hardfork.Shanghai,
-    consensus: {
-      type: ConsensusType.ProofOfStake,
-      algorithm: ConsensusAlgorithm.Casper,
-    },
-    genesis: {
-      gasLimit: 10000000000,
-      difficulty: 0,
-      nonce: '0x0000000000000000',
-      extraData: '0x0',
-    },
-    comment: 'Custom empty chain for benchmarks',
-    bootstrapNodes: [],
-    dnsNetworks: [],
-  })
-  const stateManager = new DefaultStateManager() //Creates in memory MapDB
-  const db = new MemoryLevel() as any
-  const blockData = {
-    header: {
-      parentHash: '0x000000000000000000000000000000000000000000000000000000000000000',
-      uncleHash: KECCAK256_RLP_ARRAY,
-      coinbase: Address.zero(),
-      difficulty: BigInt(0),
-      gasLimit: MAX_INTEGER_BIGINT,
-      timestamp: BigInt(1681414351),
-      extraData: '0x0',
-    },
-  }
-  const genesisBlock = Block.fromBlockData(blockData)
-  const blockchain = await Blockchain.create({ common, db, genesisBlock })
-  const eei = new EEI(stateManager, common, blockchain)
-  const evm = new EVM({ common, eei })
+  const evm = new EVM()
 
   let promiseResolve: any
   const resultPromise: Promise<Stats> = new Promise((resolve, reject) => {
@@ -77,7 +35,7 @@ async function runBenchmark(bytecode: string): Promise<Stats> {
     fn: async (deferred: any) => {
       try {
         await evm.runCode({
-          code: Buffer.from(bytecode, 'hex'),
+          code: hexToBytes('0x' + bytecode),
           gasLimit: BigInt(0xffff),
         })
         deferred.resolve()
@@ -86,7 +44,7 @@ async function runBenchmark(bytecode: string): Promise<Stats> {
       }
     },
     onCycle: (event: any) => {
-      stateManager.clearContractStorage(Address.zero())
+      evm.stateManager.clearContractStorage(Address.zero())
     },
   })
     .on('complete', () => {
